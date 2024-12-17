bafdf6c869eccc102b7f68eebbbfad222cdffe54
salshamaharani <salshabillanurmaharani@gmail.com>
2024-12-17 16:50:59 +0700

    Initial commit

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask, render_template, redirect, request, session, url_for
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
-app.secret_key = "secret_key_123"  # Jangan lupa tambahkan secret key
+app.secret_key = "secret_key"  # Tambahkan secret key untuk menggunakan session
 
 @app.route("/")
-@app.route("/<page>")
-def index(page="DataDiri"):
+def index():
+    if not session.get("logged_in"):
+        return redirect(url_for("login"))  # Jika belum login, redirect ke halaman login
+    return render_template("index.html", content_template="data_diri.html")
+
+@app.route("/daftar_nilai")
+def daftar_nilai():
     if not session.get("logged_in"):
         return redirect(url_for("login"))
-    if page == "DataDiri":
-        content_template = "data_diri.html"
-    elif page == "DaftarNilai":
-        content_template = "daftar_nilai.html"
-    else:
-        content_template = "data_diri.html"
-    return render_template("index.html", page=page, content_template=content_template)
+    return render_template("index.html", content_template="daftar_nilai.html")
+
+@app.route("/krs")
+def krs():
+    if not session.get("logged_in"):
+        return redirect(url_for("login"))
+    return render_template("index.html", content_template="krs.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("logged_in", None)  # Hapus sesi login
+    return redirect(url_for("login"))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -24,18 +34,10 @@ def login():
         if email == "salshabilla@gmail.com" and password == "1482300084":
             session["logged_in"] = True
             return redirect(url_for("index"))
+        else:
+            error = "Email atau Password salah!"  # Pesan error jika login gagal
+            return render_template("login.html", error=error)
     return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    session.pop("logged_in", None)
-    return redirect(url_for("login"))
-
-@app.route("/daftar_nilai")
-def daftar_nilai():
-    if not session.get("logged_in"):
-        return redirect(url_for("login"))
-    return render_template("index.html", page="DaftarNilai", content_template="daftar_nilai.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
